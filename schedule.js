0768263b282a700aa2e062d9ba06c99506cfe5ec
Upamanyu Das <upamanyu.das.work@gmail.com>
2021-01-09 14:15:13 +0530

    Explicitly convert time interval string to number

--- schedule.js
+++ schedule.js
@@ -2,7 +2,7 @@ const { sendMail } = require('./emailHandlers/sendMail')
 const fs = require('fs/promises')
 
 const schedule = async () => {
-  const timeLimitInMs = process.env.TIME_INTERVAL
+  const timeLimitInMs = Number(process.env.TIME_INTERVAL)
 
   try {
     const data = await fs.readFile('./data/nextMatch.json')
@@ -13,8 +13,6 @@ const schedule = async () => {
 
     const timeRemaining = matchDate - now
     console.log(`Time Remaining: ${timeRemaining}`)
-
-    console.log(typeof process.env.TIME_INTERVAL)
 
     if (timeRemaining <= timeLimitInMs && timeRemaining >= 0) {
       sendMail()
